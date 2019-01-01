d8511ec691e57f89e9a9b8fef210febe6c610c59
Jack Cherng <jfcherng@gmail.com>
2019-01-01 15:27:38 +0800

    nits
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- ass_toggle_comment.py
+++ ass_toggle_comment.py
@@ -6,7 +6,6 @@ PLUGIN_DIR = 'Packages/%s' % PLUGIN_NAME
 
 
 class AssToggleCommentCommand(sublime_plugin.TextCommand):
-
     commentPairs = [
         ('Comment: ', 'Dialogue: '),
         ('; ', ''),
@@ -15,46 +14,63 @@ class AssToggleCommentCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
 
+        for commentPoint in self._getCommentPoints():
+            for commentPair in self.commentPairs:
+                commentPairFound = False
+                pre, post = commentPair
+
+                for _ in range(2):
+                    commentRegion = sublime.Region(commentPoint, commentPoint+len(pre))
+                    commentContent = view.substr(commentRegion)
+
+                    if not commentContent.startswith(pre.rstrip()):
+                        pre, post = post, pre
+
+                        continue
+
+                    commentPairFound = True
+
+                    if commentContent != pre:
+                        commentRegion.b -= len(commentContent) - self._findFirstDiffPos(commentContent, pre)
+
+                    view.insert(edit, commentRegion.a, post)
+
+                    if not commentRegion.empty():
+                        view.erase(edit, sublime.Region(
+                            commentRegion.a + len(post),
+                            commentRegion.b + len(post)
+                        ))
+
+                    break
+
+                if commentPairFound:
+                    return
+
+    def _getCommentPoints(self):
+        view = self.view
+
         commentPoints = set()
         for region in view.sel():
             lineRegions = view.lines(region)
+
             for lineRegion in lineRegions:
                 commentPoint = view.find(r'^\s*', lineRegion.begin()).end()
                 commentPoints.add(commentPoint)
 
         # convert commentPoints into a reversely-sorted list
-        commentPoints = sorted(list(commentPoints), reverse=True)
+        return sorted(list(commentPoints), reverse=True)
 
-        for commentPoint in commentPoints:
-            for commentPair in self.commentPairs:
-                commentPairFound = False
-                pre, post = commentPair
-                for _ in range(2):
-                    commentRegion = sublime.Region(commentPoint, commentPoint+len(pre))
-                    commentContent = view.substr(commentRegion)
-                    if commentContent.startswith(pre.rstrip()):
-                        commentPairFound = True
-                        if commentContent != pre:
-                            commentRegion.b -= len(commentContent) - self.findFirstDiffPos(commentContent, pre)
-                        view.insert(edit, commentRegion.a, post)
-                        if not commentRegion.empty():
-                            view.erase(edit, sublime.Region(
-                                commentRegion.a + len(post),
-                                commentRegion.b + len(post)
-                            ))
-                        break
-                    pre, post = post, pre
-                if commentPairFound is True:
-                    break
-
-    def findFirstDiffPos (self, short, long):
+    def _findFirstDiffPos(self, short, long):
         if short == long:
             return -1
+
         if len(short) > len(long):
             short, long = long, short
+
         for i in range(len(long)):
             if i >= len(short) or short[i] != long[i]:
                 return i
+
         return len(short)
 
 
@@ -65,4 +81,5 @@ class AssToggleCommentEventListener(sublime_plugin.EventListener):
             command_name == 'toggle_comment'
         ):
             return ('ass_toggle_comment', None)
+
         return None
