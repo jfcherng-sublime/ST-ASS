a3b49a19aab0b2c47daac9577aa6c4a4a26b58cd
Jack Cherng <jfcherng@gmail.com>
2020-11-09 14:23:36 +0800

    style: format with black
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/Globals.py
+++ plugin/Globals.py
@@ -11,5 +11,6 @@ class Globals:
 
     # the regex used to match &HAABBGGRR color codes
     color_abgr_regex_obj = re.compile(
-        r"&H(?P<a>[0-9a-f]{2})?(?P<b>[0-9a-f]{2})(?P<g>[0-9a-f]{2})(?P<r>[0-9a-f]{2})\b", re.IGNORECASE,
+        r"&H(?P<a>[0-9a-f]{2})?(?P<b>[0-9a-f]{2})(?P<g>[0-9a-f]{2})(?P<r>[0-9a-f]{2})\b",
+        re.IGNORECASE,
     )
