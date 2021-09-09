9c56e75557f1346dcbdd8d744d4ac64e257ea4cf
Jack Cherng <jfcherng@gmail.com>
2021-09-09 12:21:22 +0800

    refactor: some minor optimizations
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/commands/ass_toggle_comment.py
+++ plugin/commands/ass_toggle_comment.py
@@ -1,76 +1,61 @@
 from ..helpers.functions import is_my_scope
+from ..helpers.functions import is_my_syntax
 from typing import Any, Dict, List, Optional, Set, Tuple
 import sublime
 import sublime_plugin
 
+ASS_COMMENT_PAIRS = (
+    # (before_command_executed, after_command_executed),
+    ("Comment: ", "Dialogue: "),
+    ("Dialogue: ", "Comment: "),
+    ("; ", ""),
+    ("", "; "),  # always matches
+)  # type: Tuple[Tuple[str, str], ...]
+
+
+def find_first_diff_pos(str1: str, str2: str) -> int:
+    """Finds the first difference position. Returns `-1` if both strings are identical."""
+    if str1 == str2:
+        return -1
+
+    shorter, longer = sorted((str1, str2), key=len)
+    shorter_len = len(shorter)
+
+    return next(
+        (i for i in range(shorter_len) if shorter[i] != longer[i]),
+        shorter_len,
+    )
+
+
+def get_caret_line_beginning_points(view: sublime.View) -> List[int]:
+    """Gets the points of the first non-space char of the caret lines."""
+    points = set()  # type: Set[int]
+    for region in view.sel():
+        points |= {view.find(r"^[ \t]*", line_region.begin()).end() for line_region in view.lines(region)}
+    return sorted(points)
+
 
 class AssToggleCommentCommand(sublime_plugin.TextCommand):
-    comment_pairs = (
-        ("Comment: ", "Dialogue: "),
-        ("; ", ""),
-    )  # type: Tuple[Tuple[str, str], ...]
-
     def run(self, edit: sublime.Edit) -> None:
-        for comment_point in reversed(self._get_comment_points()):
-            for comment_pair in self.comment_pairs:
-                comment_pair_found = False
-                pre, post = comment_pair
+        for beginning_point in reversed(get_caret_line_beginning_points(self.view)):
+            for before, after in ASS_COMMENT_PAIRS:
+                comment_region = sublime.Region(beginning_point, beginning_point + len(before))
+                comment_content = self.view.substr(comment_region)
+                if not comment_content.startswith(before.rstrip()):
+                    continue
 
-                for _ in range(2):
-                    comment_region = sublime.Region(comment_point, comment_point + len(pre))
-                    comment_content = self.view.substr(comment_region)
+                if comment_content != before:
+                    comment_region.b -= len(comment_content) - find_first_diff_pos(comment_content, before)
 
-                    if not comment_content.startswith(pre.rstrip()):
-                        pre, post = post, pre
-
-                        continue
-
-                    comment_pair_found = True
-
-                    if comment_content != pre:
-                        comment_region.b -= len(comment_content) - self._find_first_diff_pos(comment_content, pre)
-
-                    self.view.insert(edit, comment_region.a, post)
-
-                    if not comment_region.empty():
-                        self.view.erase(
-                            edit,
-                            sublime.Region(
-                                comment_region.a + len(post),
-                                comment_region.b + len(post),
-                            ),
-                        )
-
-                    break
-
-                if comment_pair_found:
-                    break
-
-    def _get_comment_points(self) -> List[int]:
-        comment_points = set()  # type: Set[int]
-        for region in self.view.sel():
-            comment_points |= {
-                # the point of first non-space char of the line
-                #
-                # we do not want to find \r\n
-                # because it will make the point belong to the next line
-                self.view.find(r"^[ \t]*", line_region.begin()).end()
-                for line_region in self.view.lines(region)
-            }
-
-        return sorted(comment_points)
-
-    def _find_first_diff_pos(self, str1: str, str2: str) -> int:
-        if str1 == str2:
-            return -1
-
-        shorter, longer = sorted((str1, str2), key=len)
-        shorter_len = len(shorter)
-
-        return next(
-            (i for i in range(shorter_len) if shorter[i] != longer[i]),
-            shorter_len,
-        )
+                self.view.insert(edit, comment_region.a, after)
+                self.view.erase(
+                    edit,
+                    sublime.Region(
+                        comment_region.a + len(after),
+                        comment_region.b + len(after),
+                    ),
+                )
+                break
 
 
 class AssToggleCommentEventListener(sublime_plugin.EventListener):
@@ -83,12 +68,12 @@ class AssToggleCommentEventListener(sublime_plugin.EventListener):
         if command_name != "toggle_comment":
             return None
 
-        # command only works when all target lines are in ASS scope
-        check_points = []  # type: List[int]
-        for region_selected in view.sel():
-            check_points.extend(region.begin() for region in view.lines(region_selected))
-
-        if all(is_my_scope(view, point) for point in check_points):
+        if is_my_syntax(view):
             return ("ass_toggle_comment", None)
 
-        return None
+        # command only works when all target lines are in ASS scope
+        for region_selected in view.sel():
+            for line_region in view.lines(region_selected):
+                if not is_my_scope(view, line_region.begin()):
+                    return None
+        return ("ass_toggle_comment", None)
